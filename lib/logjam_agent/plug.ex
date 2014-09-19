a0fd88afeef571efcd28018137afa9cb6451119a
Bjoern Rochel <bjoern.rochel@xing.com>
2014-09-19 14:23:39 +0200

    Remove unnecessary log message

--- lib/logjam_agent/plug.ex
+++ lib/logjam_agent/plug.ex
@@ -14,7 +14,6 @@ defmodule LogjamAgent.Plug do
      |> Conn.put_resp_header("X-Logjam-Request-Id", logjam_request_id)
      |> Conn.register_before_send(fn conn ->
           Metadata.store(code: conn.status, response_send_at: Time.now)
-          Logger.info("Response send with code #{conn.status}")
           conn
         end)
   end
