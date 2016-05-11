e5c5221448ccd624ce586d740c6ffddd13d0ce5d
David Krentzlin <david.krentzlin@xing.com>
2016-05-11 11:19:35 +0200

    Add alternative Action interface
    It aims to need minimal configuration.
    Really all you have to do for most of your controllers is `use`
    the LogjamAgent.Action module and all exported functions will be
    instrumented automatically. If you want to disable this behaviour
    for a given function you can control it via the `@logjam` module attribute.
    
    ```elixir
    defmodule MyController do
      use LogjamAgent.Action
    
      def index(conn, params) do
        # will be instrumented
      end
    
     @logjam false
      def publish(conn, params) do
        # wont be instrumented
      end
    
      def update(conn, params) do
        # will be instrumented
      end
    end
    ```

--- lib/logjam_agent/action.ex
+++ lib/logjam_agent/action.ex
@@ -1,27 +1,96 @@
 defmodule LogjamAgent.Action do
-  defmacro defaction(head, body) do
-    {fun_name, [conn, _]} = name_and_args(head)
+  @moduledoc """
+  Use this module if you want to activate Logjam reporting for your
+  Phoenix controllers. It automatically prepares all exported functions
+ in your module to submit data to the logjam service.
 
-    quote do
-      def unquote(head) do
+  ## Example:
+      defmodule UsersController do
+        use LogjamAgent.Action
+
+        def index(conn, params) do
+          # information will be reported to logjam
+        end
+
+        @logjam false
+        def update(conn, params) do
+          # this function will not report information to logjam
+        end
+      end
+
+  Note thate the `@logjam` module attribute can be used to control whether an exported function
+  shall be wired up to report to logjam. If it's set to false it will not expose logjam data.
+  """
+  defmodule Definition do
+    defstruct name: nil, args: nil, guards: nil, body: nil
+
+    def instrument_actions(mod, actions) do
+      actions
+        |> Enum.reverse
+        |> Enum.map(&augment_action(mod, &1))
+    end
+
+    defp augment_action(mod, action) do
+      Module.make_overridable(mod, [{action.name, length(action.args)}])
+      body = instrument_action(action)
+
+      if Enum.empty?(action.guards) do
+        quote do
+          def unquote(action.name)(unquote_splicing(action.args)), do: unquote(body)
+        end
+      else
+        quote do
+          def unquote(action.name)(unquote_splicing(action.args)) when unquote_splicing(action.guards), do: unquote(body)
+        end
+      end
+    end
+
+    defp instrument_action(action) do
+      [conn | _] = action.args
+
+      quote do
         env  = %{
-          module:           __ENV__.module,
-          function:         unquote(fun_name),
-          request_headers:  unquote(conn).req_headers,
-          query_string:     unquote(conn).query_string,
-          method:           unquote(conn).method
+          module:          __ENV__.module,
+          function:        unquote(action.name),
+          request_headers: unquote(conn).req_headers,
+          query_string:    unquote(conn).query_string,
+          method:          unquote(conn).method
         }
 
-        Kernel.var!(unquote(conn)) = Plug.Conn.put_resp_header(unquote(conn), "x-logjam-request-action", LogjamAgent.Transformer.logjam_action_name(env.module, env.function))
+        Kernel.var!(unquote(conn)) = Plug.Conn.put_resp_header(unquote(conn),
+                                                               "x-logjam-request-action",
+                                                               LogjamAgent.Transformer.logjam_action_name(env.module, env.function))
 
-        LogjamAgent.Buffer.instrument(env, fn ->
-          unquote(body[:do])
-        end)
+        LogjamAgent.Buffer.instrument(env, fn -> unquote(action.body) end)
       end
     end
   end
 
-  defp name_and_args({:when, _, [short_head | _]}), do: name_and_args(short_head)
-  defp name_and_args(short_head), do: Macro.decompose_call(short_head)
-end
+  defmacro __using__(_mod) do
+    quote do
+      import LogjamAgent.Action
+      Module.register_attribute(__MODULE__, :logjam_enabled_actions, accumulate: true)
 
+      @before_compile LogjamAgent.Action
+      @on_definition  LogjamAgent.Action
+    end
+  end
+
+  def __on_definition__(env, kind, name, args, guards, body)
+  def __on_definition__(%{module: mod}, :def, name, args, guards, body) do
+    unless Module.get_attribute(mod, :logjam) == false do
+      Module.put_attribute(mod, :logjam_enabled_actions, %Definition{name: name, args: args, guards: guards, body: body})
+    end
+
+    Module.delete_attribute(mod, :logjam)
+  end
+  def __on_definition__(_env, _kind, _name, _args, _guards, _body), do: nil
+
+  defmacro __before_compile__(%{module: mod}) do
+    instrumented_actions = Definition.instrument_actions(mod, Module.get_attribute(mod, :logjam_enabled_actions))
+
+    quote do
+      unquote_splicing(instrumented_actions)
+    end
+  end
+end
